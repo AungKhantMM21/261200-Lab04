e0e7c850fbdedad850822d3915e1dedb7a618283
AungKhantMM21 <139669861+AungKhantMM21@users.noreply.github.com>
2026-07-20 05:22:52 +0700

    Complete code

--- src/HealthRecord.java
+++ src/HealthRecord.java
@@ -1,3 +1,63 @@
-public class Main{
-    
+public class HealthRecord {
+    private int height;
+
+    private static final int MIN_PERMITTED_HEIGHT = 30;
+    private static final int MAX_PERMITTED_HEIGHT = 275;
+    private static final int DEFAULT_HEIGHT = 165;
+
+    private static int tallestHeight = MIN_PERMITTED_HEIGHT;
+    private static int shortestHeight = MAX_PERMITTED_HEIGHT;
+
+    private static int counter = 0;       // number of HealthRecord objects created
+    private static double averageHeight;  // running average height (cm) across all objects
+
+    public HealthRecord(int height) {
+        setHeight(height);
+        counter++;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        if (height > MIN_PERMITTED_HEIGHT && height < MAX_PERMITTED_HEIGHT) {
+            this.height = height;
+
+            if (this.height > tallestHeight) {
+                tallestHeight = this.height;
+            }
+            if (this.height < shortestHeight) {
+                shortestHeight = this.height;
+            }
+        } else {
+            this.height = DEFAULT_HEIGHT;
+        }
+
+
+        averageHeight = (averageHeight * counter + this.height) / (counter + 1);
+    }
+
+    public static int getTallestHeight() {
+        return tallestHeight;
+    }
+
+    public static int getShortestHeight() {
+        return shortestHeight;
+    }
+
+    public static double getAverageHeight() {
+        return averageHeight;
+    }
+
+    public void displayDetails() {
+        System.out.println("Height (cm): " + getHeight());
+    }
+
+    public static void displayClassDetails() {
+        System.out.println("The tallest height (cm): " + getTallestHeight());
+        System.out.println("The shortest height (cm): " + getShortestHeight());
+        System.out.printf("The average height (cm): %.2f%n", getAverageHeight());
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        HealthRecord student1 = new HealthRecord(120);
+        HealthRecord student2 = new HealthRecord(55);
+        HealthRecord student3 = new HealthRecord(280); // invalid -> defaults to 165
+
+        student1.displayDetails();
+        student2.displayDetails();
+        student3.displayDetails();
+
+        System.out.println();
+
+        HealthRecord.displayClassDetails();
+    }
 }
